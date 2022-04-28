8dd4c6d8cc52ab3ece188ac54c528a71821962e9
dazzlemon <daanill13470@gmail.com>
2022-04-28 21:31:15 +0300

    inserts done

--- lr2/create.sql
+++ lr2/create.sql
@@ -7,7 +7,6 @@ CREATE DATABASE School;
 USE School;
 
 CREATE TABLE Room( Id INT
-
                   , CONSTRAINT PK_Room PRIMARY KEY (Id)
                   )
 
@@ -67,3 +66,95 @@ CREATE TABLE Student( Id                INT  IDENTITY(1, 1)
                     , CONSTRAINT PK_Student         PRIMARY KEY (Id)
                     , CONSTRAINT FK_Student_GroupId FOREIGN KEY (GroupId) REFERENCES [Group] (Id)
                     );
+
+-- Populate
+-- source for random names: https://perchance.org/first-and-last-name
+--                 numbers: https://www.randomlists.com/random-numbers
+
+INSERT INTO Room (Id)
+          VALUES (202) --  1
+               , (303) --  2
+               , (404) --  3
+               , (504) --  4
+               , (323) --  5
+               , (220) --  6
+               , (1234) -- 7
+               , (1) --    8
+               , (419) --  9
+               , (123) -- 10
+               ;
+
+INSERT INTO Teacher (FirstName, LastName  )
+             VALUES ('John',    'Nash'    ) --  1
+                  , ('Terence', 'Tao'     ) --  2
+                  , ('fname1',  'fname2'  ) --  3
+                  , ('another', 'cool guy') --  4
+                  , ('and one', 'more'    ) --  5
+                  , ('aboba',   'abobovich') -- 6
+                  , ('Alexander',  'Ivanov') -- 7
+                  , ('Valeriy', 'Jmishenko') -- 8
+                  , ('Ryleigh', 'Smith') --     9
+                  , ('Alex', 'Gonzales') --    10
+                  , ('Elijah', 'Ashley') --    11
+                  , ('Zara', 'Kora') --        12
+                  , ('Lila', 'Gonzales') --    13
+                  , ('Amir', 'Scott') --       14
+                  ;
+
+INSERT INTO [Group] (Id, [Name], CuratorId)
+             VALUES (911, '911', 1)
+                  , (912, '912', 2)
+                  , (931, '931', 3)
+                  , (940, '940', 4)
+                  , (927, '927', 5)
+                  , (964, '964', 6)
+                  , (951, '951', 7)
+                  , (915, '915', 8)
+                  , (920, '920', 9)
+                  , (936, '936', 10)
+                  , (928, '928', 11)
+                  , (949, '949', 12)
+                  ;
+
+-- Less than 10 because there is no other types I know
+INSERT INTO ClassType ([Name])
+               VALUES ('Lecture')
+                    , ('Laboratory')
+                    , ('Practice')
+                    ;
+
+INSERT INTO Class (Name)
+           VALUES ('Game Theory')        -- 1
+                , ('Probability Theory') -- 2
+                , ('One more subject')   -- 3
+                , ('Databases') -- 4
+                , ('Translators') -- 5
+                , ('Computer Graphics') -- 6
+                , ('Operating Systems') -- 7
+                , ('Object Oriented Programming') -- 8
+                , ('User Experience') -- 9
+                , ('Mobile development') -- 10
+                ;
+
+-- Only 4 because its just combinations of other tables
+INSERT INTO Schedule ([Day], [Time], IsOddWeek, ClassTypeId, ClassId, GroupId, Subgroup, TeacherId, RoomId)
+               VALUES (0,    '9:30',  0,        1,           1,       911,     0,        1,         202)
+                    , (0,    '9:30',  1,        1,           2,       911,     0,        2,         123)
+                    , (0,    '11:00', NULL,     2,           3,       911,     1,        6,         1)
+                    , (1,    '9:30',  NULL,     2,           1,       940,     2,        1,         1234)
+                    ;
+
+INSERT INTO Student (FirstName,  LastName,     IsInFirstSubgroup, GroupId)
+             VALUES ('Daniel',    'Safonov',   0,                 911) -- 1
+                  , ('John',      'Wick',      1,                 911) -- 2
+                  , ('Valeria',   'Poverly',   1,                 931) -- 3
+                  , ('Raelynn',   'Bandini',   0,                 940) -- 4
+                  , ('Kai',       'McKenna',   1,                 927) -- 5
+                  , ('Ryker',     'Kade',      0,                 964) -- 6
+                  , ('Jesse',     'Carter',    1,                 951) -- 7
+                  , ('Barrett',   'Rivera',    1,                 915) -- 8
+                  , ('Charlotte', 'Solace',    0,                 920) -- 9
+                  , ('Bennett',   'Hernandez', 1,                 936) -- 10
+                  , ('Nyla',      'Hansley',   0,                 928) -- 11
+                  , ('Ruth',      'Kora',      0,                 949) -- 12
+                  ;
