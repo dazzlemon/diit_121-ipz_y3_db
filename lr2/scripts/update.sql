442e5ef1d4b525edcb61e5ba3bc71b65a01012c0
dazzlemon <daanill13470@gmail.com>
2022-05-01 12:24:00 +0300

    2.2

--- lr2/scripts/update.sql
+++ lr2/scripts/update.sql
@@ -1,1 +1,13 @@
 -- TODO
+USE School
+
+DROP TABLE IF EXISTS Student
+
+CREATE TABLE Student( Id                INT  IDENTITY(1, 1)
+                    , FirstName         TEXT NOT NULL
+                    , LastName          TEXT NOT NULL
+                    , GroupId           INT  NOT NULL
+                    
+                    , CONSTRAINT PK_Student         PRIMARY KEY (Id)
+                    , CONSTRAINT FK_Student_GroupId FOREIGN KEY (GroupId) REFERENCES School.[Group] (Id)
+                    )
