d82088486dc39c992b237411c246e9138291a3f4
dazzlemon <daanill13470@gmail.com>
2022-05-01 17:14:49 +0300

    2.3

--- lr2/scripts/update.sql
+++ lr2/scripts/update.sql
@@ -11,3 +11,16 @@ CREATE TABLE Student( Id                INT  IDENTITY(1, 1)
                     , CONSTRAINT PK_Student         PRIMARY KEY (Id)
                     , CONSTRAINT FK_Student_GroupId FOREIGN KEY (GroupId) REFERENCES School.[Group] (Id)
                     )
+
+ALTER TABLE Student ADD NewColumn INT NULL
+
+-- ALTER TABLE Student SP_RENAME COLUMN FirstName TO FirstNameRenamed -- DOESNT WORK IN T-SQL
+EXEC sp_rename 'Student.FirstName', 'FirstNameRenamed', 'COLUMN'
+
+ALTER TABLE Student
+    ALTER COLUMN FirstNameRenamed NVARCHAR(6)
+
+ALTER TABLE Student
+    ADD CONSTRAINT DF_Student_FirstNameRenamed DEFAULT 'aboba' FOR FirstNameRenamed
+
+ALTER TABLE Student DROP DF_Student_FirstNameRenamed
