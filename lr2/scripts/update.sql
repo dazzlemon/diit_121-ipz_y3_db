32a3fea7352e13256de9b3a1c08c8d7c82e0f7f5
dazzlemon <daanill13470@gmail.com>
2022-05-01 17:34:42 +0300

    2.4

--- lr2/scripts/update.sql
+++ lr2/scripts/update.sql
@@ -24,3 +24,30 @@ ALTER TABLE Student
     ADD CONSTRAINT DF_Student_FirstNameRenamed DEFAULT 'aboba' FOR FirstNameRenamed
 
 ALTER TABLE Student DROP DF_Student_FirstNameRenamed
+
+UPDATE Teacher
+    SET FirstName = 'baobab'
+    WHERE CONVERT(VARCHAR(MAX), FirstName) = 'aboba'
+
+UPDATE Teacher
+    SET FirstName = 'Avatar'
+    Where CONVERT(VARCHAR(MAX), FirstName) = 'Zara'
+
+INSERT INTO Student (FirstNameRenamed,  LastName,     GroupId)
+             VALUES ('Daniel', 'Safonov',   911) -- 1
+                  , ('John',   'Wick',      911) -- 2
+                  , ('Valery', 'Poverly',   931) -- 3
+                  , ('Raelyn', 'Bandini',   940) -- 4
+                  , ('Kai',    'McKenna',   927) -- 5
+                  , ('Ryker',  'Kade',      964) -- 6
+                  , ('Jesse',  'Carter',    951) -- 7
+                  , ('Baret',  'Rivera',    915) -- 8
+                  , ('4arlot', 'Solace',    920) -- 9
+                  , ('Bennet', 'Hernandez', 936) -- 10
+                  , ('Nyla',   'Hansley',   928) -- 11
+                  , ('Ruth',   'Kora',      949) -- 12
+                  ;
+
+UPDATE Student
+    SET FirstNameRenamed = 'Avatar'
+    Where CONVERT(VARCHAR(MAX), FirstNameRenamed) = 'Ruth'
