27126a97465d5da4544dc21085489617b7029890
dazzlemon <daanill13470@gmail.com>
2022-05-14 23:18:46 +0300

    top, percent, with ties

--- lr3/select.sql
+++ lr3/select.sql
@@ -13,14 +13,14 @@ SELECT DENSE_RANK() OVER( PARTITION BY [Day]
                           ORDER BY [Time] ASC
                         ) AS "row" 
      ,"Day" = CASE [Day] WHEN 0 THEN 'Monday'
-                          WHEN 1 THEN 'Tuesday'
-                          WHEN 2 THEN 'Wednesday'
-                          WHEN 3 THEN 'Thursday'
-                          WHEN 4 THEN 'Friday'
-                          WHEN 5 THEN 'Saturday'
-                          WHEN 6 THEN 'Sunday'
-                          ELSE 'IMPOSSIBRU'
-                          END
+                         WHEN 1 THEN 'Tuesday'
+                         WHEN 2 THEN 'Wednesday'
+                         WHEN 3 THEN 'Thursday'
+                         WHEN 4 THEN 'Friday'
+                         WHEN 5 THEN 'Saturday'
+                         WHEN 6 THEN 'Sunday'
+                         ELSE 'IMPOSSIBRU'
+                         END
      , [Time]
      , IsOddWeek
      , ClassTypeId
@@ -43,3 +43,8 @@ SELECT GroupId, COUNT(*) AS StudentCount
 FROM Student
 GROUP BY GroupId
 HAVING COUNT(*) = 2
+
+-- SELECT top 10% students with highest marks
+SELECT TOP 10 PERCENT WITH TIES *
+FROM Student
+ORDER By Id -- imagine that they are ordered by theirs marks or something
