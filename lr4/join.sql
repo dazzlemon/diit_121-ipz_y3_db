95842871d21194dc5d74bbf902ff100e197f46e2
dazzlemon <daanill13470@gmail.com>
2022-05-15 19:08:37 +0300

    except + better comments

--- lr4/join.sql
+++ lr4/join.sql
@@ -1,13 +1,25 @@
 USE School
 
+-- Schedule with ClassId replaced with actual ClassName
 -- cartesian product
-SELECT *
+SELECT Day, Time, IsOddWeek, ClassTypeId, Name AS ClassName, GroupId, Subgroup
+     , TeacherId, RoomId
 -- FROM Schedule, Class -- Same
 FROM Schedule CROSS JOIN Class
+WHERE Class.Id = ClassId
 
+-- Show all active classes
 -- intersect
      SELECT ClassId
      FROM Schedule
 INTERSECT
+     SELECT Id
+     FROM Class
+
+-- Show all inactive classes
+-- except
+     SELECT ClassId
+     FROM Schedule
+EXCEPT
      SELECT Id
      FROM Class
