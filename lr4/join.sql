a6f9b75a72c3b6c0a95ea11a557a0794d0e165ea
dazzlemon <daanill13470@gmail.com>
2022-05-15 19:05:17 +0300

    intersect

--- lr4/join.sql
+++ lr4/join.sql
@@ -2,4 +2,12 @@ USE School
 
 -- cartesian product
 SELECT *
+-- FROM Schedule, Class -- Same
 FROM Schedule CROSS JOIN Class
+
+-- intersect
+     SELECT ClassId
+     FROM Schedule
+INTERSECT
+     SELECT Id
+     FROM Class
