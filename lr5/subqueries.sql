304a8c291a4183f4eb4383250242f7c54af9793b
dazzlemon <daanill13470@gmail.com>
2022-10-23 13:04:38 +0300

    first subquery and some = any

--- lr5/subqueries.sql
+++ lr5/subqueries.sql
@@ -1,85 +1,33 @@
-USE School
+-- Multi valued self contained suqbuery
+-- In
 
--- Schedule with ClassId replaced with actual ClassName
--- cartesian product
-SELECT Day, Time, IsOddWeek, ClassTypeId, Name AS ClassName, GroupId, Subgroup
-     , TeacherId, RoomId
--- FROM Schedule, Class -- Same
-FROM Schedule CROSS JOIN Class
-WHERE Class.Id = ClassId
-
--- Show all active classes
--- intersect
-     SELECT ClassId
-     FROM Schedule
-INTERSECT
-     SELECT Id
-     FROM Class
-
--- Show all inactive classes
--- except
-     SELECT ClassId
-     FROM Schedule
-EXCEPT
-     SELECT Id
-     FROM Class
-
--- Idk real world example
-     SELECT ClassId
-     FROM Schedule
-UNION
-     SELECT Id
-     FROM Class
-
-SELECT DISTINCT COALESCE(ClassId, Class.Id) AS "Class ID"
-FROM Schedule FULL JOIN Class
-ON ClassId = Class.Id
-
-SELECT COALESCE(ClassId, Class.Id) AS "Class ID"
-FROM Schedule FULL JOIN Class
-ON ClassId = Class.Id
-
-SELECT ClassId, Class.Id
-FROM Schedule FULL JOIN Class
-ON ClassId = Class.Id
-
-
--- Idk real world example
-     SELECT ClassId
-     FROM Schedule
-UNION ALL
-     SELECT Id
-     FROM Class
-
--- inner join
-SELECT Day, Time, IsOddWeek, ClassTypeId, Name AS ClassName, GroupId, Subgroup
-     , TeacherId, RoomId
--- FROM Schedule, Class -- Same
-FROM Schedule JOIN Class
-ON Class.Id = ClassId
-
--- Idk real world example
-SELECT *
+-- Shows all classes on any day between monday and friday that is lecture
+-- or laboratory
+-- Days are mapped to text
+-- also row_number added
+SELECT DENSE_RANK() OVER( PARTITION BY [Day]
+                          ORDER BY [Time] ASC
+                        ) AS "row" 
+     , "Day" = CASE [Day] WHEN 0 THEN 'Monday'
+                          WHEN 1 THEN 'Tuesday'
+                          WHEN 2 THEN 'Wednesday'
+                          WHEN 3 THEN 'Thursday'
+                          WHEN 4 THEN 'Friday'
+                          WHEN 5 THEN 'Saturday'
+                          WHEN 6 THEN 'Sunday'
+                          ELSE 'IMPOSSIBRU'
+                          END
+     , [Time]
+     , IsOddWeek
+     , ClassTypeId
+     , ClassId
+     , GroupId
+     , Subgroup
+     , TeacherId
+     , RoomId
 FROM Schedule
-JOIN Room
-ON RoomId >= Room.Id
-
--- multi join
-SELECT Day, Time, IsOddWeek, ClassType.Name AS "Class type"
-     , Class.Name AS ClassName, GroupId, Subgroup, TeacherId, RoomId
--- FROM Schedule, Class -- Same
-FROM Schedule
-JOIN Class ON Class.Id = ClassId
-JOIN ClassType ON ClassType.Id = ClassTypeId
-
-SELECT Class.Id
-FROM Class
-LEFT JOIN Schedule ON ClassId = Class.Id
-
-SELECT Class.Id
-FROM Class
-RIGHT JOIN Schedule ON ClassId = Class.Id
-
-SELECT Class.Id
-FROM Class
-FULL JOIN Schedule ON ClassId = Class.Id
+WHERE [Day] BETWEEN 0 AND 4 -- between monday and friday (inclusive)
+AND ClassTypeId IN ( SELECT Id
+                     FROM ClassType
+                     WHERE [Name] LIKE 'L%' -- 'Lecture' or 'Laboratory'
+                   )
